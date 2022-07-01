783e7caf2ff09e9ebfb1f868b9d27c0e89b36c43
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-06-30 18:30:43 -0700

    becoming annoying

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
+#!/bin/bash
 #include <stdio.h>
 /*
-* main - entry
-* Return - Always 0 (success)
+* main - Main  entry
+* Return: Always 0 (Success)
 	*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
