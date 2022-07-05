49792c7e1ce6cf2054db74058e9af450ed60a8c7
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-04 19:07:21 -0700

    finally

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints possible outcomes of three digit
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = 48; n <= 57; n++)
+		{
+			putchar(
+ch);
+			putchar(n);
+			if (ch != 57 || n != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
