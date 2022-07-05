03db57a3d5ee0e0ca6b4943d2d56cb7d4c05ea85
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-04 17:48:20 -0700

    fhhhh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@ int main(void)
 	int ch;
 	int n;
 
-	for (ch == 48; ch <= 57; ch++)
+	for (ch = 48; ch <= 57; ch++)
 	{
-		for (n == 49; n <= 57; n++)
+		for (n = 49; n <= 57; n++)
 		{
 			if (n > ch)
 			{
 				putchar(ch);
 				putchar(n);
-				if (n != 56 || ch != 57)
+				if (n != 56 && ch != 57)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
