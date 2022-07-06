79e8e5eec34a2ea32d4adcc66712945fe3712b04
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-06 02:27:46 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <main.h>
 /**
  * main - a program that prints _putchar
@@ -14,6 +15,7 @@ int main(void)
 	_putchar("h");
 	_putchar("a");
 	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
