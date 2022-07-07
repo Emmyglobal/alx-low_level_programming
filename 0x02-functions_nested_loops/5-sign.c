9cd69e73a75370bc0121692a374d65840b6a8eea
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-06 20:46:14 -0700

    goog

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a nuber
+ * @n : character to check
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	int check;
+
+	if (n > 0)
+	{
+		check = 1;
+		_putchar('+');
+	}
+		else if (n == 0);
+		{
+			check = 0;
+			_putchar('0');
+		}
+	else
+	{
+		check = -1;
+		_putchar('-');
+	}
+	return (check);
+}
