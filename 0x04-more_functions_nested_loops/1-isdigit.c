c0d0206e094517d9d5a20d9a8f2d66e2a67f4790
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-08 21:18:44 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isdigit - function to check for a digit
+ * _isdigit - function to check for a digit that runs from 0 through 9
  * @c: an integer argument
- * Rrturn: success
+ * Return: success
  */
 int _isdigit(int c)
 {
-	return (c <= '0' && c >= '9');
+	return (c >= '0' && c <= '9');
 }
