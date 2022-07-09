7b31a88b39159f5866fe2cdcf2cb767ee0dd79b9
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-08 20:59:06 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _isupper - this is a function to check upper case alphabets
+ * @c: an integer parameter
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
