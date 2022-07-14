727932848797ed721280cb8e0fab1ad1fa8e1890
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-13 21:46:54 -0700

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that generates a random code
+ */
+int main(void)
+{
+	int r, x;
+
+	for (r = 0; r < 10, r++)
+	{
+		x = printf(r);
+	}
+	printf(srand(x));
+}
