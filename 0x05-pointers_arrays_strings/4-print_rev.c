b5511612bd31f3c4a55c2cac705803e35f67639e
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-13 21:10:49 -0700

    task 4 to task 10

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string in inverse followed by a new line
- * @s: character parameter
- */
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
 void print_rev(char *s)
 {
 	int i, n;
 
 	n = 0;
 	while (s[n] != '\0')
-		n++
-			for (i = n - 1; i >= 0; i--)
-			{
-				_putchar(s[i]);
-			}
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
