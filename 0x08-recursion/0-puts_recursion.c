222051259a6a78fb3c2d95e6603ee88cf9d8e216
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-21 02:15:19 -0700

    fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
  * _puts_recursion - a function that prints a string.
  * followed by a new line
  * @s: character parameter
- * Return: 0;
  */
 void _puts_recursion(char *s)
 {
