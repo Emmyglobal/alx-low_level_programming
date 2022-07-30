8025d49326c68e05b612c68aabc8d3ef913295cb
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-07-29 19:29:23 -0700

    task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - function that allocates memory using malloc
- * @unsigned int b: parameter
- * returna a pointer to the allocated memory
+ * malloc_checked - function that allocates memory using a pointer
+ * @b:the number of bytes to be allocate
+ * return a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
