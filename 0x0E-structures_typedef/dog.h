66a110dc32490f6f400b0013cff9425ca16203cb
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-08-02 15:45:53 -0700

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - a structure dog with some elements
- * struct - the struct dog
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
 struct dog
 {
 	char *name;
