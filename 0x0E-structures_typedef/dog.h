c7ba6153dfbdbda2cd4b3cd9d14171689844e0eb
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-08-02 21:56:26 -0700

    fixing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * struct dog - gives information about dog
@@ -15,5 +15,7 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
