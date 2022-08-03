785253c783edc7b4556dc13a9a4f15a98dcc2fa5
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-08-02 18:42:53 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * struct dog - gives information about dog
  * @name: name of dog
@@ -14,5 +13,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
