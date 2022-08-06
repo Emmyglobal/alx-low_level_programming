217d54680fcce4f225c06ccea4312ebcbda27b1d
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-08-05 20:16:23 -0700

    fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,12 @@
 #include "function_pointers.h"
 
 /**
- * void print_name -> functions that prints name
+ * print_name -> functions that prints name
  * @name : name of person
+ * @f: function printer
  * return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	printf("Hello, my name is %s\n", name);
+	printf("Hello, my name is: %s\n", name);
 }
