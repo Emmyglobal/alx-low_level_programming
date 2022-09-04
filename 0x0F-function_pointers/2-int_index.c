4fd7123d585d15f861c763c4a108a524293b085a
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-09-04 08:49:08 -0700

    hot fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (size <= 0)
 		return (-1);
 	if (array == NULL || cmp == NULL)
 		return (-1);
-	for (int index = 0; index < size; c++)
+	for (index = 0; index < size; c++)
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
