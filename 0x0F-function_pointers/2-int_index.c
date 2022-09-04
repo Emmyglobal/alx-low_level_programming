3ed51b0c3e8f7e27a7f1c2af81abe03ce0d3e000
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2022-09-04 07:05:15 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index -> function to search a number in an array
+ * @array: array parameter
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used
+ * return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	scanf("%d", &cmp);
+	for (int c = 0; c < size; c++)
+	{
+		if (array[c] == cmp)
+		{
+			printf("%d \n", cmp);
+			break;
+		}
+		return (0);
+	}
+	if (int c == size || size <= 0)
+		printf("failed");
+	return (-1);
+}
