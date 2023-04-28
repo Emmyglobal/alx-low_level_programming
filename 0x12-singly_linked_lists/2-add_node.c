a83266b932b51e0eaf267cb57f7c341b04e48c75
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-04-28 00:19:40 -0700

    error fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,21 +5,47 @@
  * @s: string
  * Return: length of the string
  */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node - > Function that add new nodes to the list
+ * @head: current place in the list
+ * @str: string to add to the head
+ * Return: pointer to current position in list
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
+	int i, len;
+	char *content;
 	list_t *node_to_be_added = malloc(sizeof(list_t));
 
-	if (node_to_be_added == NULL)
+	if (str == NULL || node_to_be_added == NULL)
 	{
 		return (NULL);
 	}
+	len = _strlen(str);
 	node_to_be_added->str = strdup(str);
+	content = malloc((len + 1) * sizeof(char));
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
 	if (node_to_be_added->str == NULL)
 	{
 		free(node_to_be_added);
+		free(content);
 		return (NULL);
 	}
+	node_to_be_added->len = len;
+	node_to_be_added->str = content;
 	node_to_be_added->next = *head;
 	*head = node_to_be_added;
+
 	return (node_to_be_added);
 }
