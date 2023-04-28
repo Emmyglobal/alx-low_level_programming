52b04b7cf30cb224fd22be3c7ec9a23440f89afb
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-04-28 16:08:22 -0700

    done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - >  function that frees a list_t list.
+ * @head: The firrst node on the list.
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node = head;
+	while (node != NULL) {
+		list_t *next = node->next;
+		free(node->str);
+		free(node);
+		node = next;
+	}
+}
+
