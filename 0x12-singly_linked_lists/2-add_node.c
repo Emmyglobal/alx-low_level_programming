7c198145e548a3cb2b8db58b06cd1d95e1dd4dd2
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-04-27 22:53:17 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,4 +5,18 @@
  * @s: string
  * Return: length of the string
  */
+list_t *add_node(list_t **head, const char *str)
+{
+	str->next = head;
+	head = str;
+
+	if (success)
+	{
+		return (**head);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
 
