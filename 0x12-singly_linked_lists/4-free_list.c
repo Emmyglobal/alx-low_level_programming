b9d634ace0f2d828e99e8db07482a10870ea4f1b
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-04-28 19:28:57 -0700

    Hot fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,16 @@
  * @head: The firrst node on the list.
  * Return: Nothing
  */
-
 void free_list(list_t *head)
 {
 	list_t *node = head;
-	while (node != NULL) {
+
+	while (node != NULL)
+	{
 		list_t *next = node->next;
+
 		free(node->str);
 		free(node);
 		node = next;
 	}
 }
-
