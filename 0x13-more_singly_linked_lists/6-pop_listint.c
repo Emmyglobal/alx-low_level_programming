ec2dc21b791835bc7d4f2130b684039edd590239
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-03 00:14:52 -0700

    fixing

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,12 @@
 
 int pop_listint(listint_t **head)
 {
+	listint_t *temp;
+
 	if (head == NULL)
 		return (0);
 	else
 	{
-		listint_t *temp;
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
