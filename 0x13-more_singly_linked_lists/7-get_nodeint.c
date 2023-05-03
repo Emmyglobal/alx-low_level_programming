fce8568989c8340816790da6eef7bb7939baac8a
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-03 00:44:13 -0700

    Hot fit get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 
 	listint_t *current;
-	current = head;
 
+	current = head;
 	while (current != NULL)
 	{
 		if (count == index)
