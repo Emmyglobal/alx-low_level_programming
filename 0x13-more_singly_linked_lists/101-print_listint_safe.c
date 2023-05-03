cd25a153d1fc8b7c7470099d2c2d698e51ee0a07
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-02 21:31:56 -0700

    A function that frees

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe -Function that prints a linked list
+ * @head: list of type to print
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num_node = 0;
+	long int diff;
+
+	while (head != NULL)
+	{
+		diff = head - head->next;
+		num_node++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->n);
+			break;
+		}
+	}
+	return (num_node);
+}
