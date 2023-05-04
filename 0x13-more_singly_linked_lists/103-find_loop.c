9d6689883cb0e865a10c8bb614fc9fa170754301
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-03 18:10:33 -0700

    Fixing

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -15,7 +15,7 @@ listint_t *find_listint_loop(listint_t *head)
 	if (!head)
 		return (NULL);
 
-	while (slow && fast && fas->next)
+	while (slow && fast && fast->next)
 	{
 		fast = fast->next->next;
 		slow = slow->next;
