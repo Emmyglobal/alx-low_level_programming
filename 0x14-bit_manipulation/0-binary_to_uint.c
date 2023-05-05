e2b3adab067f3103e0c70a18461843ed20439581
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-04 19:39:11 -0700

    conversion

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,10 +17,8 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		{
 			return (0);
-			converted_integer = 2 * converted_integer + (b[i] - '0');
-		}
+		converted_integer = 2 * converted_integer + (b[i] - '0');
 	}
 	return (converted_integer);
 }
