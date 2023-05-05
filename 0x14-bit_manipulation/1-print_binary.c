bb05b3b17012d43eb971d36d0877eb736b21e152
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-04 21:37:48 -0700

    counts the number of bits to change

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#inlude "main.h"
+
+/**
+ * print_binary -> Prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, counts = 0;
+	unsigned long int current;
+
+	for (i = 63; 1 >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			counts++;
+		}
+		else if (counts)
+			_putchar('0');
+	}
+	if (!counts)
+		_putchar('0');
+}
