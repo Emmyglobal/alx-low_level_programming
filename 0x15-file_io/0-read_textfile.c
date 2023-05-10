ecb57e057813007c4a3916ae23a0eba0c1b46b41
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-10 06:47:48 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * read_textfile -> Function that reads a text file and prints it to the POSIX standard output.
+ * @filename: Pointer to the file name.
+ * @letters: number of letter it could read or print
+ * Return: 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *filename;
+	filename = fopen(filename, "r", "w");
+
+	if (file == NULL)
+		return (0);
+
+	fclose(*filename);
+}
