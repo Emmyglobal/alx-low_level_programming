16968de81277cf1219757b3a118295d4830f6fb2
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-12 23:48:11 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,11 +11,27 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *filename;
-	filename = fopen(filename, "r", "w");
+	ssize_t fread, fwrite;
+	char *totalSize;
+	FILE *file;
 
-	if (file == NULL)
+	totalSize = malloc(sizeof(char) * letters);
+	if (totalSize == NULL)
+		return (0);
+	if (filename == NULL)
 		return (0);
 
-	fclose(*filename);
+	file = fopen(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+	fread = read(file, totalSize, letters);
+	if (fread == -1)
+		return (0);
+	fwrite = write(STDOUT_FILENO, totalSize, fread);
+	if (fwrite == -1)
+		return (0);
+	close(file);
+	free(totalSize);
+	return (fwrite);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 
 
 int _putchar(char c);
