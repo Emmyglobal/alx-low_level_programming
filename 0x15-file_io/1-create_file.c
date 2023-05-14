e822a6d8793083dd63e9f9aa2ecc85a7c8be3317
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-13 21:51:40 -0700

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++);
-		fwrite = write(file, text_content,len);
+		for (len = 0; text_content[len]; len++)
+			;
+		fwrite = write(file, text_content, len);
 		if (fwrite == -1)
 		{
 			close(file);
