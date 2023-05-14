ab8feea58cd6ff7f8edf6c7e87913fd98886d4db
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-13 21:33:48 -0700

    modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file -> Function that create file
@@ -9,11 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file = fopen(filename, "w");
-	fputs(text_content, file);
+	int file;
 
 	if (filename == NULL)
 		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		size_t len = strlen(text_content);
+		ssize_t ret = write(file, text_content,len);
+		if (ret == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
 	return (1);
-	fclose(file);
 }
