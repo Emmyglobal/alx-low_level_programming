61e41a6278c1ceb7a21c8f99846d9b28da8ab352
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-14 13:42:15 -0700

    fixing

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,9 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
 
+	if (filename == NULL)
+		return (-1);
+
 	file = open(filename, O_APPEND | O_RDWR);
 	if (file == -1)
 		return (-1);
