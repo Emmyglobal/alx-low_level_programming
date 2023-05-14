a183eeb2545050c844ed8a619bfe5e95a5561a42
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-13 20:53:56 -0700

    Crete a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
+ * create_file -> Function that create file
+ * @filename: name of file created
+ * @text_content: content of text
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *file = fopen(filename, "w");
+	fputs(text_content, file);
+
+	if (filename == NULL)
+		return (-1);
+	return (1);
+	fclose(file);
+}
