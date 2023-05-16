2c0ab097a171200ceda2eb226beb6aff445196e5
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-15 23:59:09 -0700

    hot fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -22,6 +22,29 @@ int main(int argc, char **argv)
 	}
 	Elf64_Ehdr header;
 	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
 		fprintf(stderr, ""Error Failed to read ELF header: %sn, strerror(errno));
 	exit(98);
+	}
+	if (memcmp(header.e_idnt, ELFMAG, SELFMAG) != 0)
+	{
+		fprintf(stderr, "Error: NOT an ELF filen");
+		exit(98);
+	}
+	printf(" Magic: ");
+	for (int i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header.e_ident[i]);
+	}
+	printf(" Class: %sn" header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :  "ELF32");
+	printf(" Data: %sn", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf(" Version: %d (current)n", header.e_ident[EI_VERSION]);
+	printf(" OS/ABI: %sn", header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+	printf(" ABI Version: %dn", header.e_ident[EI_ABIVERSION]);
+	printf(" Type: %sn", header.e_type == ET_EXEC ? "EXEC (Executable file)" : (header.e_type == ET_DYN ? "DYN (Shared object file)" : "Other"));
+	printf(" Entry point address: 0x%lxn", header.e_entry);
+
+	close(fd);
+
+	return 0;
 }
