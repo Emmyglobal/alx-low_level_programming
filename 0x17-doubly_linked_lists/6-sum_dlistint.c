5142b13271faeca3ac8e97ce32356b4067e1432b
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-06-16 14:02:50 -0700

    return sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint -> returns sum of allthe data of a linked list
+ * @head: pointer to the first node
+ * Return: 0 if the list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	unsigned int sum = 0;
+
+	while (head != NULL)
+	{
+		current = current->next;
+		sum++;
+	}
+	return (sum);
+}i
