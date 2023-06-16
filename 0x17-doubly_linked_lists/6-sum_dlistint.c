f1aaca1dde036a7945cccada9ec4ce7cd95e8014
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-06-16 14:18:19 -0700

    return sum fixed

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,12 +11,9 @@ int sum_dlistint(dlistint_t *head)
 	dlistint_t *current = head;
 	int sum = 0;
 
-	if (head = NULL)
-		return (0);
-
 	while (current != NULL)
 	{
-		sum++;
+		sum += current->n;
 		current = current->next;
 	}
 	return (sum);
