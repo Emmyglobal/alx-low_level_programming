76c9d2582ab88a75380ba1c0cb68b24491c12eea
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-06-15 21:36:02 -0700

    fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
-  * print_dlistint - prints a doubly linked list
-  * @h: Head of the list
-  * Return: Number of items in the linked list
-  */
+ * print_dlistint - prints a doubly linked list
+ * @h: Head of the list
+ * Return: Number of items in the linked list
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
