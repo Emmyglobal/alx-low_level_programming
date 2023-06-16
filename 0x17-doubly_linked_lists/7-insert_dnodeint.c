78f5ef1d5cabe8c44460be5ebd382d1aed41afea
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-06-16 15:03:13 -0700

    adds to any point

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,33 +10,33 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint *first = *h, *new_node;
-	unsigned int i = 0;
+    dlistint_t *new_node, *current = *h;
+    unsigned int i = 0;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
+    if (idx == 0)
+        return (add_dnodeint(h, n));
 
-	while (first != NULL && i < idx - 1)
-	{
-		first = first->next;
-		i++;
-	}
+    while (current != NULL && i < idx - 1)
+    {
+        current = current->next;
+        i++;
+    }
 
-	if (first == NULL)
-		return (NULL);
+    if (current == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->n = n;
-	new_node->prev = first;
-	new_node->next = first->next;
+    new_node->n = n;
+    new_node->prev = current;
+    new_node->next = current->next;
 
-	if (first->next != NULL)
-		first->next->prev = new_node;
+    if (current->next != NULL)
+        current->next->prev = new_node;
 
-	first->next = new_node;
+    current->next = new_node;
 
-	return (new_node);
+    return (new_node);
 }
