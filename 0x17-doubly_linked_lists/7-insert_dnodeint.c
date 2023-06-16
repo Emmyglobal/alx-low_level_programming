66b7f2264fdccc4f79e37063d4cc63bd9039bca4
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-06-16 15:10:47 -0700

    adds to any point fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,10 +12,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current = *h;
 	unsigned int i = 0;
-	
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	
+
 	while (current != NULL && i < idx - 1)
 	{
 		current = current->next;
@@ -23,19 +23,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (current == NULL)
 		return (NULL);
-	
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->prev = current;
 	new_node->next = current->next;
-	
+
 	if (current->next != NULL)
 		current->next->prev = new_node;
-	
+
 	current->next = new_node;
-	
+
 	return (new_node);
 }
