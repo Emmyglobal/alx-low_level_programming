cb239e3ca1643e2d606302da4ebcaa6029474c56
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-06-25 14:10:24 -0700

    main.h

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - function to check for a digit that runs from 0 through 9
+ * @c: an integer argument
+ * Return: success
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - check for lower case letters
+ * @c: character to check the lower case
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
