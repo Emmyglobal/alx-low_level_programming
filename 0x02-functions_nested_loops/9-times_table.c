3c6868a6ee09e38ad40e172390ea654a79062096
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-01 16:40:39 -0700

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,39 +7,31 @@
 
 void times_table(void)
 {
+	int num1, num2, mul;
 
-int a = 0;
-int b;
-int rep;
-while (a <= 9)
-{
- b = 0;
- while (b <= 9)
- {
-  rep = a * b;
-  if (b == 0)
-  {
-   _putchar('0' + rep);
-  }
-  else if (rep < 10)
-  {
-   _putchar(' ');
-   _putchar('0' + rep);
-  }
-  else
-  {
-   _putchar('0' + rep / 10);
-   _putchar('0' + rep % 10);
-  }
-
-  if (b < 9)
-  {
-   _putchar(',');
-   _putchar(' ');
-  }
-  b++;
- }
- _putchar('\n');
- a++;
-}
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			mul = num1 * num2;
+			if (mul >= 10)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else if (mul < 10 && num2 != 0)
+			{
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+			else
+				_putchar(mul + '0');
+			if (num2 != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
