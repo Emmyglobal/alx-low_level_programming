631c242e0452c18ae5f77c34af38e5d02889aeed
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-02 04:41:41 -0700

    Fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,24 +3,24 @@
 
 /*
  * print_sum_mul_3 -> prints the sum of all the multiples of 3 0r 5
- * Return: Nothiing
+ * Return: Nothing
  */
 
-void print_sum_mul_3()
+void print_sum_mul_3(void)
 {
 	int i, sum = 0;
 
-	for (i = 1; i < 1024; i++)
+	for (i = 1; i < 10; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 		{
 			sum += i;
 		}
 	}
-	printf("The sum is %d\n", sum);
+	printf("%d\n", sum);
 }
 
-int main()
+int main(void)
 {
 	print_sum_mul_3();
 	return (0);
