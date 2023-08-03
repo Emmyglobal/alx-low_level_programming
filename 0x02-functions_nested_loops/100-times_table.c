ad25c7950c94dd2e48bc74a41df52ad5c7faa915
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-03 15:06:21 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,34 @@
 #include "main.h"
 
 /**
- * print_times_table -> prints the n times table, starting with 0
- * @n: n times table
- * Return: Nothing
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The n times table.
+ *
+ * Return: Nothing.
  */
-
 void print_times_table(int n)
 {
-	int num1, mul, num2;
-
+	int num1, num2, mul;
+	
+	if (n < 0 || n > 15)
+		return;
 	for (num1 = 0; num1 <= n; num1++)
 	{
 		for (num2 = 0; num2 <= n; num2++)
 		{
-			if(n < 15 && n > 0)
+			mul = num1 * num2;
+			if (mul >= 100)
+				_putchar((mul / 100) + '0');
+			if (mul >= 10)
+				_putchar(((mul / 10) % 10) + '0');
+			_putchar((mul % 10) + '0');
+			if (num2 != n)
 			{
-				mul = num1 * num2;
-				if (mul >= 10 && mul < 100)
-				{
-					_putchar((mul / 10) + '0');
-					_putchar((mul % 10) + '0');
-				}
-				else if (mul >= 100)
-				{
-					_putchar((mul / 100) + '0');
-					_putchar(((mul / 10) % 10) + '0');
-					_putchar((mul % 10) + '0');
-				}
-				else if (mul < 10 && num2 != 0)
-				{
+				_putchar(',');
+				_putchar(' ');
+				if (num1 * (num2 + 1) < 10)
 					_putchar(' ');
-					_putchar(mul + '0');
-				}
-				else
-					_putchar(mul + '0');
-				if (num2 != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
-			else
-				break;
 		}
 		_putchar('\n');
 	}
