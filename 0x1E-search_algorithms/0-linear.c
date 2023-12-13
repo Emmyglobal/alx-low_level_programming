a732c23caed2f3b4ea89ec23650d5faa7195fdfe
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-12-13 03:49:22 -0800

    Betty sorted

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * int linear_search - > Searches for a value in an array of integers
+ * linear_search - > Searches for a value in an array of integers
  * using linear search
  * @array: array pointer
  * @size: size of array
@@ -13,6 +13,7 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
