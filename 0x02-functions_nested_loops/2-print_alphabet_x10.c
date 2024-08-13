4d0623d5b3257f8ddaa61c33a136e260c03dfc25
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-08-13 10:08:14 -0700

    10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 
-/*
- * print_alphabet_x10 -> prints a to z 10 times
- * Return: Nothing
+/**
+ * print_alphabet_x10 -> the function prototype
+ * Description: prints 10 times the alphabet in lowercase
+ *
+ * Return: void
  */
-
 void print_alphabet_x10(void)
 {
-	int count;
-	char letter;
+	int i = 0, j;
 
-	for (count = 0; count < 10; count++)
+	while (i <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
 		_putchar('\n');
+		i++;
 	}
 }
