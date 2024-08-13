7d5dc2ee2625057cbca3d67a977e37e9208c4c35
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-08-13 09:49:07 -0700

    pushed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - a program that prints _putchar
- * followed by new line
- * Return: Always 0 (successful)
+ * main -> Entry point
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
