4912865c95df58831449d154607ea6296a546187
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-08-13 10:51:17 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- * print_sign - check numbers
- * @n : number to check
- * Return: 0 or 1
+ * print_sign -> Prints the sign of a function
+ * @n: The number whose sign is to be printed
+ *
+ * Return: 1 if +ve, 0 if zero and -1 if -ve
  */
 int print_sign(int n)
 {
@@ -16,9 +18,10 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
+	return (1);
 }
