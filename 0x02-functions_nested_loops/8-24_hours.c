20004bd3bc648a7b3189a683391e2c4ce4b2c8e1
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-08-13 22:09:39 -0700

    fixed the j part

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,6 +2,7 @@
 
 /**
  * jack_bauer -> Prints every minute of the day of Jack Bauer
+ * Description: prints the time spent.
  *
  * Return: Nothing
  */
@@ -10,8 +11,10 @@ void jack_bauer(void)
 	int i, j, k, l;
 	for (i = 48; i <= 50; i++ )
 	{
-		for (j = 48; j <= 51; j++)
+		for (j = 48; j <= 57; j++)
 		{
+			if (i == 50 && j > 51)
+				break;
 			for (k = 48; k <= 53; k++)
 			{
 				for (l = 48; l <= 57; l++)
@@ -22,9 +25,9 @@ void jack_bauer(void)
 					_putchar(k);
 					_putchar(l);
 					_putchar('\n');
+					
 				}
 			}
 		}
 	}
 }
-
