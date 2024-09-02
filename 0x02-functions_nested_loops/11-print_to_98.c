3a9ce7558e8903dc7df0dfec9bc5ebeb43df51e9
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-09-02 07:10:42 -0700

    reduced the number of lines

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- *  * print_to_98 - Prints all natural numbers from n to 98
- *   * @n: The starting number to print from
- *    *
- *     * Return: void
- *      */
+ *  print_to_98 - Prints all natural numbers from n to 98
+ *  @n: The starting number to print from
+ *
+ * Return: void
+ */
+
 void print_to_98(int n)
 {
 	int i;
