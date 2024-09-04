81dbd8e8c6e37229df5eab7eb93bce5f59070b9f
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-09-03 23:09:50 -0700

    push sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-#include "main.h"
 
-/*
- * print_sum_mul_3 - prints the sum
- * Description: sum of multiples of 3 0r 5 below 1024
- * Return: void.
- */
-
-void print_sum_mul_3(void)
-{
-	int i, sum = 0;
-
-	for (i = 1; i < 1024; i++)
-	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-		{
-			sum += i;
-		}
-	}
-	printf("%d\n", sum);
-}
-
-/*
- * main - runs the header function
- * Description: a function that runs our function
- * Return: Always 0.
+/**
+ * main -> adds up the multiple of 3 and 15 between 0 and 1024
+ *
+ * Return: 0 as Success always
  */
 
 int main(void)
 {
-	print_sum_mul_3();
-	return (0);
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if((i % 3) == 0 || (i % 5) == 0)
+		{
+			j += i;
+		}
+	}
+	printf("%d\n", j);
+	return(0);
 }
