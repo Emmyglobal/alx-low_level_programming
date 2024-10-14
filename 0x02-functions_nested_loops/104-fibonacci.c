e70bae4c429d225a8c5418f8ae9a467da3a3d234
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-14 03:47:41 -0700

    first 98 fibonacci sequence corrected again

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,21 @@
 
 int main(void)
 {
-	unsigned long int i, next, first = 1, second = 2;
+	unsigned long int i, high = 0, high1 = 0, next, nexth, first = 1, second = 2;
 
-	printf("%lu, %lu, ", first, second);
+	printf("%lu, %lu", first, second);
 	for (i = 0; i <= 95; i++)
 	{
-		next = first + second;
-		printf("%lu", next);
-		if (i != 95)
-			printf(", ");
+		next = (first + second) % 10000000000;
+		nexth = high + high1 + (first + second) / 10000000000;
+		if (nexth > 0)
+			printf(", %lu%010lu", nexth, next);
+		else
+			printf(", %lu", next);
 		first = second;
+		high = high1;
 		second = next;
+		high1 = nexth;
 	}
 	printf("\n");
 	return (0);
