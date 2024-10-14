0a8360adb20203b2693327ee4ed255cd7e2cb5e2
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-14 02:53:39 -0700

    sum of fibonacci corrected

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- * Main -> finds and prints the sum of the even-valued terms.
+ * Main -> Entry point
+ * Description: finds and prints the sum of the even-valued terms.
  *
  * Return : Always 0 for success
  */
 
 int main(void)
 {
-	long int i, sum, next, first = 0, second = 1;
+	long int i, sum = 0, next, first = 0, second = 1;
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i <= 50; i++)
 	{
 		next = first + second;
 		if ((next <= 4000000) && (next % 2 == 0))
 		{
-			sum =+ next;
+			sum += next;
 		}
 		first = second;
 		second = next;
