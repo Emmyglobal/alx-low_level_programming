27a08317a6695563064a26108d72c7fd07d5f53b
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-14 02:20:52 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,44 +1,26 @@
 #include <stdio.h>
-#include "main.h"
 
-/*
- * print_fibo - prints fibonacci sequence
- * Description: starts from 1 and 2 and prints first 50
- * Return: void.
- */
-
-void print_fibo(void)
-{
-	unsigned long next, first = 1, second = 2;
-	int i;
-
-	next = first + second;
-
-	printf("%lu, %lu, ", first, second);
-
-	for (i = 3; i <= 50; i++)
-	{
-		if (i < 50)
-		{
-			printf("%lu, ", next);
-			first = second;
-			second = next;
-			next = first + second;
-		}
-		else
-			printf("%lu", next);
-	}
-}
-
-/*
- * main - Entry point
- * Description: prints fibonacci
- * Return: Always 0;
+/**
+ * main -> Entry point
+ * Description: program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+ *
+ * Return: always 0 for success
  */
 
 int main(void)
 {
-	print_fibo();
+	long int i, next, first = 1, second = 2;
+
+	printf("%ld, %ld, ", first, second);
+	for (i = 0; i <= 48; i++)
+	{
+		next = second + first;
+		printf("%ld", next);
+		if (i != 48)
+			printf(", ");
+		first = second;
+		second = next;
+	}
 	printf("\n");
 	return (0);
 }
