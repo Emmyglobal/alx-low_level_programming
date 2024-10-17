31bc3d65c1a0c04700bd708c793b5a686b7da215
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-17 14:33:17 -0700

    FizzBuzz problem solved

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5) == 0)
-		{
-			printf("FizzBuzz");
-			if (i != 100)
-				printf(" ");
-		}
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
 		{
-			printf("Fizz");
+			printf("Buzz");
 			if (i != 100)
 				printf(" ");
 		}
-		else if ((i % 5) == 0)
-			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
