505a30e56586014a445e1f8e8cf6aafd79ae5e49
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-18 23:21:32 -0700

    prime factors and integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - > Prints an integer
+ * @n: integer to be printed
+ * Return: Null
+ */
+
+void print_number(int n)
+{
+	if (n < 1.0 / 0.0)
+		printf("%d\n", n);
+}
