a9b004c243f108a4dbef4a14cc58581fe2b8b121
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-21 06:42:50 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,23 +7,14 @@
 
 int main(void)
 {
-	unsigned long int i, m = 612852475143, largest_prime = 0;
+	unsigned long int i = 3;
+	unsigned long int m = 612852475143;
 
-	while (m % 2 == 0)
-	{
-		largest_prime = 2;
-		m = m / 2;
-	}
-	for (i = 3; i * i <= m; i += 2)
+	for (; i < 12057; i += 2)
 	{
 		while (m % i == 0 && m != i)
-		{
-			largest_prime = i;
-			m = m / i;
-		}
+			m /= i;
 	}
-	if (m > 1)
-		largest_prime = m;
-	printf("The highest prime number is %lu\n", largest_prime);
+	printf("The highest prime factor is: %lu\n", m);
 	return (0);
 }
