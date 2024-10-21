ed708904dccfbe94d4263bd0ac08d588a4ff4a69
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-21 06:58:55 -0700

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,8 +8,14 @@
 
 void print_number(int n)
 {
-	double inf = 1.0 / 0.0;
-
-	if (n < inf)
-		printf("%d\n", n);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
