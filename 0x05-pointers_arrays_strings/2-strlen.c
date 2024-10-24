ac8dd3a31499b131ce1aee438d87433400e6bf2e
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-24 05:09:50 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen -> returns the length of a string.
+ * @s: string to be returned
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return len;
+}
