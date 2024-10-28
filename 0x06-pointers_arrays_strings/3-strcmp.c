9c06e6ca3ea63170a38abf293222c6c929e67abf
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-10-28 06:38:54 -0700

    compares

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp -> Compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0 for success and positive or -ve if different.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int len = 0;
+
+	while(s1[len] != '\0' && s2[len] != '\0')
+	{
+		if (s1[len] != s2[len])
+			return (s1[len] - s2[len]);
+		len++;
+	}
+	return(s1[len] - s2[len]);
+}
