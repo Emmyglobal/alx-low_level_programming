ce8008bdaf2570392b056095e0e614eac2058c49
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-11-04 06:55:21 -0800

    print it

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
 #include "main.h"
-#include <string.h>
 
 /**
-* _memset -> this memory set function
-* @s: string
-* @b: a charcter
-* @n: an integer
-* Return:  a string
-*/
+ * _memset -> fills memory with a constant byte
+ * @s: charater to fill
+ * @b: constant byte
+ * @n: number of memory area
+ * Return: a pointer to the memory area s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
+	unsigned int i;
 
-	for (a = 0; a < n; a++)
-		s[a] = b;
-
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,8 @@
-This the folder for pointer to pointer
+# 0x07. C - Even more pointers, arrays and strings
+
+## using C language
+
+## GENERAL
+- What are pointers to pointers and how to use them
+- What are multidimensional arrays and how to use them
+- What are the most common C standard library functions to manipulate strings
