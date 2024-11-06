c2a1044fa17b294479ff6ad4d3538bef853cd211
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-11-06 05:45:56 -0800

    print chessboard

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,17 +11,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j;
 
+	if (needle[i] == '\0')
+		return (haystack);
 	while (haystack[i] != '\0')
 	{
 		j = 0;
 		while (needle[j] != '\0')
 		{
-			if (haystack[i] == needle[0])
-			{
-				return (&haystack[i]);
-			}
+			if (haystack[i + j] != needle[j])
+				break;
 			j++;
 		}
+		if (needle[j] == '\0')
+			return &haystack[i];
 		i++;
 	}
 	return (NULL);
