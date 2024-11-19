a74f9ef94b1e200a54f76cf7fa301c63323e82ed
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-11-19 04:07:27 -0800

    join two strings and create space for them

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+/**
+ * string_nconcat -> concatenates two string
+ * @s1: first string
+ * @s2: second string
+ * @n: malloc size
+ *
+ * Return: pointer to the allocated memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *pta;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	pta = malloc(len1 + (n < len2 ? n : len2) + 1);
+	if (pta == NULL)
+	{
+		exit(1);
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		pta[i] = s1[i];
+		i++;
+	}
+	while ( j < n && s2[j] != '\0')
+	{
+		pta[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	pta[i] = '\0';
+	return (pta);
+}
