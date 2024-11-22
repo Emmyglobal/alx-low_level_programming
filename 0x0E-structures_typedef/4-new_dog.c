528211d4bbec419c32b7532b370fcc60fc3a8072
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-11-22 03:29:43 -0800

    hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,25 +15,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *pta;
 
 	pta = malloc(sizeof(dog_t));
-	if (pta != NULL)
+	if (pta == NULL)
+		return (NULL);
+
+	pta->name = malloc(strlen(name) + 1);
+	if (pta->name == NULL)
 	{
-		pta->name = malloc(strlen(name) + 1);
-		if (pta->name == NULL)
-		{
-			free(pta);
-			return (NULL);
-		}
-		strcpy(pta->name, name);
-
-		pta->age = age;
-
-		pta->owner = malloc(strlen(owner) + 1);
-		if (pta->owner == NULL)
-		{
-			free(pta);
-			return (NULL);
-		}
-		strcpy(pta->owner, owner);
+		free(pta);
+		return (NULL);
 	}
+	strcpy(pta->name, name);
+	pta->age = age;
+
+	pta->owner = malloc(strlen(owner) + 1);
+	if (pta->owner == NULL)
+	{
+		free(pta->name);
+		free(pta);
+		return (NULL);
+	}
+	strcpy(pta->owner, owner);
+
 	return (pta);
 }
