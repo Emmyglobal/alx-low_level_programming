c63b1544bb7d8ba7eb6271365f188c05409266b4
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-11-21 23:13:23 -0800

    fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog -> creates a new dog
@@ -16,9 +17,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	pta = malloc(sizeof(dog_t));
 	if (pta != NULL)
 	{
-		pta->name = name;
+		pta->name = malloc(strlen(name) + 1);
+		if (pta->name == NULL)
+		{
+			free(pta);
+			return (NULL);
+		}
+		strcpy(pta->name, name);
+
 		pta->age = age;
-		pta->owner = owner;
+
+		pta->owner = malloc(strlen(owner) + 1);
+		if (pta->owner == NULL)
+		{
+			free(pta);
+			return (NULL);
+		}
+		strcpy(pta->owner, owner);
 	}
 	return (pta);
 }
