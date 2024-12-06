6d4081e99356e8b12a9a6a73e8f1946cd41dd183
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-06 19:47:11 +0100

    hot fix again

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,8 @@
  * add_node -> add node at the beginning of a linked list
  * @head: head of the node containing the address of the list
  * @str: the string to be added
+ * 
+ * Return: The new node or NULL
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -13,16 +15,18 @@ list_t *add_node(list_t **head, const char *str)
     new = (list_t *)malloc(sizeof(list_t));
     if(new == NULL)
         return (NULL);
+
     new->str = strdup(str);
     new->len = strlen(str);
+
     if (new->str == NULL)
     {
         free(new);
         return (NULL);
     }
+
     new->next = *head;
     *head = new;
 
     return (new);
-
 }
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_node_end -> Add a new node at the end of a list_t list
+ * @head: the head of the list
+ * @str: string to be added
+ * 
+ * Return: Address of the new element or NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    
+}
