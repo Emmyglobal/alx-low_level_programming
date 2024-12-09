4f184a7b0fe4b87fef5f22b7f0f2278e7debe871
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-09 09:19:41 +0100

    betty style corrected

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,11 @@
 #include "lists.h"
-
+#include <string.h>
 /**
- * add_node -> add node at the beginning of a linked lis
- * @head: head of the node containing the address of the list
- * @str: the string to be added
- * Return: The new node or NULL
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node(list_t **head, const char *str)
