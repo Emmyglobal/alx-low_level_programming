73200b1ca2c3c2f69f24c00e09be83f09e45595a
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-09 09:35:21 +0100

    betty style corrected newly

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,13 +11,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	size_t count;
 
 	new = (list_t *)malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	new->len = strlen(str)
+
+	for (count = 0; str[count]; count++)
+		;
+	new->len = count;
 	new->next = *head;
 	*head = new;
-	return (new);
+	return (*head);
 }
