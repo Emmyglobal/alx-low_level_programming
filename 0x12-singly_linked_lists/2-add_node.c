f8800e7313545343c1bdd421ac28fe71bf912a4c
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-09 09:04:17 +0100

    betty style corrected

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,24 +4,22 @@
  * add_node -> add node at the beginning of a linked list
  * @head: head of the node containing the address of the list
  * @str: the string to be added
- * 
+ *
  * Return: The new node or NULL
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
+	list_t *new;
 
-    new = (list_t *)malloc(sizeof(list_t));
-    new->str = strdup(str);
-    new->len = strlen(str);
-    new->next = NULL;
+	new = (list_t *)malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
 
-    if (!new)
-	    return (NULL);
-
-    new->next = *head;
-    *head = new;
-
-    return (new);
+	if (!new)
+		return (NULL);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
