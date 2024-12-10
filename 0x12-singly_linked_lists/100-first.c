8788621f48fb25e644faf1639f62cee3e77722c4
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-10 09:58:14 +0100

    using __attribute__((constructo(constructor)) to execute my function first before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * main -> Entry point
+ *
+ * Return: Nothing
+ */
+void __attribute__((constructor)) first(void)
+{
+	int i = 0;
+	char sentence[] = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+
+	while (sentence[i] != '\0')
+	{
+		_putchar(sentence[i]);
+		i++;
+	}
+}
