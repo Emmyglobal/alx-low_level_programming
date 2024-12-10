533a52773845c435e0aec27886de0b44beb1f4b3
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-10 10:18:44 +0100

    runs before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,19 +1,24 @@
 #include "lists.h"
 
 /**
- * main -> Entry point
+ * first -> This function prints first before the main function does
  *
  * Return: Nothing
  */
 void __attribute__((constructor)) first(void)
 {
-	int i = 0;
-	char sentence[] = "(You're beat! and yet, you must allow,\nI bore) "
-			  "(my house upon my back!\n)";
+	int i = 0, j = 0;
+	char sentence[] = "You're beat! and yet, you must allow,\n";
+	char sent[] = "I bore my house upon my back!\n";
 
 	while (sentence[i] != '\0')
 	{
 		_putchar(sentence[i]);
 		i++;
+	}
+	while (sent[j] != '\0')
+	{
+		_putchar(sent[j]);
+		j++;
 	}
 }
