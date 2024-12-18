55afa45b6d31a78e63b5105ddf92f16d7b3c224e
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-18 17:42:00 +0100

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,13 +10,22 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t bytes_written = 0;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY, 0644);
-	if (fd == NULL)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
