5471866a17a2310955d4ce5d54d86e02ec09c489
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-26 14:39:02 +0100

    count the number of element

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,23 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * dlistint_len - Gets length of the list
- * @h: The head of the list
- * Return: Number of items in the linked list
+ * print_dlistint -> prints all the elements of a dlistint_t list
+ * @h: the head of the node
+ * Return: Number of nodes
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i;
+    int noOfNodes;
 
-	for (i = 0; h; i++)
-		h = h->next;
-	return (i);
+    noOfNodes = 0;
+    while (h != NULL)
+    {
+        h = h->next;
+        noOfNodes++;
+    }
+
+    return noOfNodes;
 }
